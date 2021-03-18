522dfbfa467dab6709f0ee4f34974a9f73148677
fanta <>
2021-03-18 17:44:35 +0800

    no message

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,5 +1,4 @@
 (function webpackUniversalModuleDefinition(root, factory) {
-  debugger;
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
 	else if(typeof define === 'function' && define.amd)
